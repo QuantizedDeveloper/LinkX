3bbd644b75e940990844e565f5f786efead4d01d
QuantizedDeveloper <quantizeddeveloper@gmail.com>
2026-06-16 13:52:22 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,6 +14,8 @@ self.addEventListener("notificationclick", (event) => {
   event.notification.close();
 
   event.waitUntil(
-    clients.openWindow("/inbox")
+    clients.openWindow("/LinkX/#/inbox")
   );
 });
+
+
